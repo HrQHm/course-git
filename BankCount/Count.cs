9da84fe1ab51bb0d8e495d4e405e19ca0a374eb7
Henrique Hiroshi Makita <henrique_hmk@hotmail.com>
2019-12-29 09:52:08 -0300

    Projeto de Exercicio C#

---
+++ BankCount/Count.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+    
+namespace BankCount {
+    class Count {
+        public int NumberCount  { get; private set; }
+        public double Balance { get; private set; }
+        public string Name { get; set; }
+
+        public Count(int numberCount, string name) { //construtor
+            NumberCount = numberCount;
+            Name = name;
+        }
+
+        public Count(int numberCount, string name, double balance) : this(numberCount, name){
+            Balance = balance;
+        }
+
+        public void DepositoSaldo (double valor){
+            Balance += valor;
+        }
+
+        public void SaqueSaldo(double valor) {
+            Balance -= valor + 5.00;
+        }
+        public override string ToString() {
+            return "Conta " + NumberCount +
+                   ", Titular: " + Name + ", Saldo: $ " +
+                   Balance.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ BankCount/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+namespace BankCount {
+    class Program {
+        static void Main(string[] args) {
+
+            Count countClass;
+
+            Console.WriteLine("Gold Saucer Bank - Welcome!");
+            Console.Write("Digite número da conta: "  );
+            int numberCount = int.Parse(Console.ReadLine());
+            Console.Write("Digite o titular da conta: ");
+            string nameCount = Console.ReadLine();
+            Console.Write("Haverá Depósito Inicial (s/n)? ");
+            char op = char.Parse(Console.ReadLine());
+
+            if (op == 's' || op == 'S') {
+                Console.Write("Entre o valor do depósito incial: ");
+                double balanceCount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                countClass = new Count(numberCount, nameCount, balanceCount);//carregando variaveis da classe
+            }
+            else 
+                countClass = new Count(numberCount, nameCount);//carregando variaveis da classe
+            
+            
+            
+            Console.WriteLine(countClass);
+            Console.WriteLine("-----------------------------------------");
+
+            Console.Write("Entre um valor para depósito: ");
+            countClass.DepositoSaldo(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            Console.WriteLine(countClass);
+
+            Console.WriteLine("-----------------------------------------");
+            Console.Write("Entre um valor para saque: ");
+            countClass.SaqueSaldo(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            Console.WriteLine(countClass);
+
+
+
+        }
+    }
+}
